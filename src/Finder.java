3afe7309f667399bb4237604e3ae276d1956d69c
Shel Keller <mkeller12@austincollege.edu>
2016-02-16 18:52:48 -0600

    Filter object is now finished but still needs JUnit tests.

--- src/Finder.java
+++ src/Finder.java
@@ -1,10 +1,13 @@
+import java.io.IOException;
+import java.util.ArrayList;
 
 
-//Object jobs: 
-//Receives a list of strings
-//Creates a list of strings that are real words
-//using the dictionary
-//returns said list
+
+
 public class Finder {
-
+	
+	
+	
+	
+	
 }
