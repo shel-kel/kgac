885deca57de566ef58938a12a22ec822adb81968
Davis Gregory-Allen <dgregoryallen15@austincollege.edu>
2016-02-21 11:24:23 -0600

    Added skeleton of Controller. Need at least Constructor for View to go any further.

--- PuzzleSolver/src/Controller.java
+++ PuzzleSolver/src/Controller.java
@@ -1,6 +1,13 @@
 //Holds an instance of Filter, Finder, and View
 //Receives information from Filter and Finder to pass to View
 
-public class Controller {
-
+public class Controller
+{
+	public static void main(String[] args)
+	{
+		Filter fil = new Filter();
+		View v = new View();
+		String[][] puzzle = v.getMatrix();//placeholder until we can get the method to get the puzzle from the user.
+		Finder fin = new Finder(puzzle);
+	}
 }
