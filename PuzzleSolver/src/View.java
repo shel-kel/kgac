40404896c94d1f1405aa5681670c9160d2a75265
Chris Cooper <ccooper14@austincollege.edu>
2016-02-22 08:51:40 -0600

    More

--- PuzzleSolver/src/View.java
+++ PuzzleSolver/src/View.java
@@ -13,6 +13,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+
 import javax.swing.JPanel;
 import java.awt.GridLayout;
 import javax.swing.JLabel;
@@ -27,6 +29,7 @@ import java.awt.Component;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.List;
 
 public class View extends JFrame {
 	private JTable tableMat;
@@ -36,7 +39,8 @@ public class View extends JFrame {
 	Object[] results;
 	private int matrixArea;
 	private JLabel lblNewLabel_1;
-	private JTextField textField;
+	private JTextField matrixField;
+	private String[][][] matrixList;
 	public View() {
 		
 		JPanel panel = new JPanel();
@@ -46,7 +50,6 @@ public class View extends JFrame {
 		JLabel lblNewLabel = new JLabel("Size: ");
 		panel.add(lblNewLabel);
 		
-		
 		String[] sizeList = { "3", "4", "5", "6", "7", "8", "9" };
 		
 		JComboBox sizeBox = new JComboBox(sizeList);
@@ -55,7 +58,7 @@ public class View extends JFrame {
 		JLabel lblNewLabel_2 = new JLabel("Length:");
 		panel.add(lblNewLabel_2);
 		
-		String[] lengthList = { "3", "4", "5", "6", "7", "8", "9" };
+		Object[] lengthList = { "3", "4", "5", "6", "7", "8", "9" };
 		
 		JComboBox lengthBox = new JComboBox(lengthList);
 		panel.add(lengthBox);
@@ -64,16 +67,12 @@ public class View extends JFrame {
 		getContentPane().add(panel_1, BorderLayout.CENTER);
 		panel_1.setLayout(new GridLayout(0, 1, 0, 0));
 		
-		lblNewLabel_1 = new JLabel("Matrix: (Enter numbers seperated by commas)");
+		lblNewLabel_1 = new JLabel("Matrix: (Enter letters seperated by commas)");
 		panel_1.add(lblNewLabel_1);
 		
-		textField = new JTextField();
-		panel_1.add(textField);
-		textField.setColumns(10);
-		
-//		this.tableMat = new JTable();
-//		this.tableMat.setSize(this.matrixArea, this.matrixArea);
-//		panel_1.add(this.tableMat);
+		matrixField = new JTextField();
+		panel_1.add(matrixField);
+		matrixField.setColumns(10);
 		
 		JPanel panel_2 = new JPanel();
 		getContentPane().add(panel_2, BorderLayout.SOUTH);
@@ -92,8 +91,16 @@ public class View extends JFrame {
 	protected void onSubmitBtnClicked(){
 		int x = 0;
 		int y = 0;
+		String[][] matrix = new String[x][y];
 		int matrixSize = Integer.valueOf((String) this.sizeBox.getSelectedItem());
-		
+		String matrixString = this.matrixField.getText();
+		java.util.List<String> this.matrixList = Arrays.asList(matrixString.split(","));
+		matrixList = 
+		while (x != matrixSize) {
+			while (y != matrixSize) {
+				matrix = this.matrixList[0];
+			}
+		}
 	
 	}
 
